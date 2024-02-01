b677095f4a7d681b4a7abcc2088e9cdf712f085c
simonwinbladnti <152278872+simonwinbladnti@users.noreply.github.com>
2024-02-01 14:21:29 +0100

    Localstorage implementerat
    Nytt sätta att lägga till uppgifter

--- js/script.js
+++ js/script.js
@@ -18,7 +18,40 @@ selectBurger.addEventListener('click', showMenu);
 let addTasks = document.querySelector(".add-task");
 
 function addTask() {
-    console.log("hej");
-}
+    let menu = document.querySelector(".add-tasks");
+    if (!menuActive) {
+        menu.classList.add("add-tasks-show");
+        menuActive = true;
+    } else {
+        menu.classList.remove("add-tasks-show");
+        menuActive = false;
+    }}
 
 addTasks.addEventListener('click', addTask);
+
+let submitButton = document.querySelector("#submit");
+
+submitButton.addEventListener('click', submitForm);
+
+function submitForm() {
+    let data = {        
+         desc: document.getElementById("description").value,
+         label: document.getElementById("label").value,
+         day: document.getElementById("days").value,
+    };
+    createTask(data);
+    localStorage.setItem("taskData", JSON.stringify(data));
+}
+
+function createTask(data) {
+    let newTaskElement = document.createElement("div");
+    newTaskElement.className = "task";
+
+    newTaskElement.innerHTML = `<h4>${data.label}</h4><p>${data.desc}</p>`;
+
+    let dayContainer = document.querySelector(`.${data.day.toLowerCase()} .calendar-tasks`);
+    
+    dayContainer.appendChild(newTaskElement);
+}
+
+submitButton.addEventListener('click', submitForm)
